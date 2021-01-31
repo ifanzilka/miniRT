598a192da7480fd8f37e4d3020540889a894f3de
ifanzilka <nabiyarov.fanzil@gmail.com>
2021-01-31 15:21:15 +0500

    norm main

--- miniRT/srcs/main.c
+++ miniRT/srcs/main.c
@@ -6,13 +6,12 @@
 /*   By: bmarilli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 19:26:13 by bmarilli          #+#    #+#             */
-/*   Updated: 2021/01/27 22:11:54 by bmarilli         ###   ########.fr       */
+/*   Updated: 2021/01/31 15:20:56 by ifanzilka        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_minirt.h>
 #include <parse.h>
-
 
 /*
 **  checking the validity of an argument
@@ -24,12 +23,12 @@ static int	ft_check_argv(int argc, char **argv)
 		ft_error(dont_find);
 	else if (argc > 3)
 		ft_error(lots_arg);
-	if (ft_strlen(argv[1]) > 3 && ft_strnstr(argv[1] + ft_strlen(argv[1]) 
-		- 3,".rt", ft_strlen(argv[1])))
+	if (ft_strlen(argv[1]) > 3 && ft_strnstr(argv[1] + ft_strlen(argv[1])
+		- 3, ".rt", ft_strlen(argv[1])))
 	{
 		if (argc == 2)
 			return (1);
-		else if (ft_strlen(argv[2]) == 6 && !ft_strncmp(argv[2], 
+		else if (ft_strlen(argv[2]) == 6 && !ft_strncmp(argv[2],
 			"--save", 6))
 			return (1);
 		else
