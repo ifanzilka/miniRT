eeecc3e73e0b31318661ee579601bc3c9d18c2ad
Blood Marillion <bmarilli@er-c3.kzn.21-school.ru>
2021-02-01 17:23:02 +0300

    add norm vec2

--- miniRT/srcs/vectors/vec_operation.c
+++ miniRT/srcs/vectors/vec_operation.c
@@ -6,7 +6,7 @@
 /*   By: bmarilli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 17:02:46 by bmarilli          #+#    #+#             */
-/*   Updated: 2021/02/01 17:02:47 by bmarilli         ###   ########.fr       */
+/*   Updated: 2021/02/01 17:22:40 by bmarilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,70 +15,67 @@
 /*
 **  Operation minus  vector xyz
 **
-**   a - b 
+**   a - b
 **  return -> vector xyz
 */
 
-
-t_xyz   ft_xyz_minus(t_xyz a, t_xyz b)
+t_xyz	ft_xyz_minus(t_xyz a, t_xyz b)
 {
-    t_xyz res;
+	t_xyz	res;
 
-    
-    res.x = a.x - b.x;
-    res.y = a.y - b.y;
-    res.z = a.z - b.z;
-    return (res);
+	res.x = a.x - b.x;
+	res.y = a.y - b.y;
+	res.z = a.z - b.z;
+	return (res);
 }
 
 /*
 **  Operation plus  vector xyz
 **
-**   a + b 
+**   a + b
 **  return -> vector xyz
 */
 
-t_xyz   ft_xyz_plus(t_xyz a, t_xyz b)
+t_xyz	ft_xyz_plus(t_xyz a, t_xyz b)
 {
-    t_xyz res;
+	t_xyz res;
 
-    res.x = a.x + b.x;
-    res.y = a.y + b.y;
-    res.z = a.z + b.z;
-    return (res);
+	res.x = a.x + b.x;
+	res.y = a.y + b.y;
+	res.z = a.z + b.z;
+	return (res);
 }
-
 
 /*
 **  Operation mult  vector xyz
 **
-**   a * b 
+**   a * b
 **  return -> vector xyz
 */
 
-t_xyz   ft_xyz_mult_db(t_xyz a, double b)
+t_xyz	ft_xyz_mult_db(t_xyz a, double b)
 {
-    t_xyz res;
+	t_xyz res;
 
-    res.x = a.x * b;
-    res.y = a.y * b;
-    res.z = a.z * b;
-    return (res);
+	res.x = a.x * b;
+	res.y = a.y * b;
+	res.z = a.z * b;
+	return (res);
 }
 
 /*
 **  Operation div  vector xyz
 **
-**   a / b 
+**   a / b
 **  return -> vector xyz
 */
 
-t_xyz   ft_xyz_div_db(t_xyz a, double b)
+t_xyz	ft_xyz_div_db(t_xyz a, double b)
 {
-    t_xyz res;
+	t_xyz	res;
 
-    res.x = a.x / b;
-    res.y = a.y / b;
-    res.z = a.z / b;
-    return (res);
+	res.x = a.x / b;
+	res.y = a.y / b;
+	res.z = a.z / b;
+	return (res);
 }
