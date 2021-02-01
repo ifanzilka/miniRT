69c667e27ba901896cd066f5773026352f278479
Blood Marillion <bmarilli@er-c3.kzn.21-school.ru>
2021-02-01 19:04:55 +0300

    NORM PARSE.H

--- miniRT/includes/parse.h
+++ miniRT/includes/parse.h
@@ -6,30 +6,29 @@
 /*   By: bmarilli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 22:25:55 by bmarilli          #+#    #+#             */
-/*   Updated: 2020/12/12 22:28:33 by bmarilli         ###   ########.fr       */
+/*   Updated: 2021/02/01 19:02:23 by bmarilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef PARSE_H
-#define PARSE_H
-#include <ft_minirt.h>
-#include <math.h>
+#ifndef PARSE_H
+# define PARSE_H
+# include <ft_minirt.h>
+# include <math.h>
 
-
-int         ft_parse_rt(char *file,int argc);
-t_rgb       ft_atoirgb(char *str,int *i);
-int         ft_check_rgb(t_rgb rgb);
-int         ft_check_normalizate(t_xyz xyz);
-t_xyz       ft_atoi_xyz(char *str, int *i);
-int         ft_check_xyz(t_xyz xyz_t);
-int         ft_parse_r(t_rt *rt,char *str);
-int         ft_parse_a(t_rt *rt,char *str);
-int         ft_parse_c(t_rt *rt,char *str);
-int         ft_parse_l(t_rt *rt,char *str);
-int         ft_parse_sp(t_rt *rt,char *str);
-int         ft_parse_cy(t_rt *rt,char *str);
-int         ft_parse_pl(t_rt *rt , char *str);
-int         ft_parse_sq(t_rt *rt , char *str);
-int         ft_parse_tr(t_rt *rt , char *str);
+int		ft_parse_rt(char *file, int argc);
+t_rgb	ft_atoirgb(char *str, int *i);
+int		ft_check_rgb(t_rgb rgb);
+int		ft_check_normalizate(t_xyz xyz);
+t_xyz	ft_atoi_xyz(char *str, int *i);
+int		ft_check_xyz(t_xyz xyz_t);
+int		ft_parse_r(t_rt *rt, char *str);
+int		ft_parse_a(t_rt *rt, char *str);
+int		ft_parse_c(t_rt *rt, char *str);
+int		ft_parse_l(t_rt *rt, char *str);
+int		ft_parse_sp(t_rt *rt, char *str);
+int		ft_parse_cy(t_rt *rt, char *str);
+int		ft_parse_pl(t_rt *rt, char *str);
+int		ft_parse_sq(t_rt *rt, char *str);
+int		ft_parse_tr(t_rt *rt, char *str);
 
 #endif
