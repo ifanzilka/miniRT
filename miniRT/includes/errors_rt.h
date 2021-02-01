8217b54ea67371ef003338bbf97aa1a7a053daf0
Blood Marillion <bmarilli@er-c3.kzn.21-school.ru>
2021-02-01 18:01:40 +0300

    norm errors.h

--- miniRT/includes/errors_rt.h
+++ miniRT/includes/errors_rt.h
@@ -6,36 +6,34 @@
 /*   By: bmarilli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 14:18:25 by bmarilli          #+#    #+#             */
-/*   Updated: 2021/01/27 14:18:27 by bmarilli         ###   ########.fr       */
+/*   Updated: 2021/02/01 18:01:22 by bmarilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_RT_H
 # define ERRORS_RT_H
 
-//#include <ft_minirt.h>
-
 typedef	enum
 {
-    no_er,
-    dont_find,
-    lots_arg,
-    err_r = 4,
-    err_a,
-    err_rt,
-    err_c,
-    err_l,
-    err_sp,
-    err_cy,
-    err_sq,
-    err_tr,
-    err_mlx,
-    err_malloc,
-    err_rgb,
-    err_pl,
-    err_argv
+	no_er,
+	dont_find,
+	lots_arg,
+	err_r = 4,
+	err_a,
+	err_rt,
+	err_c,
+	err_l,
+	err_sp,
+	err_cy,
+	err_sq,
+	err_tr,
+	err_mlx,
+	err_malloc,
+	err_rgb,
+	err_pl,
+	err_argv
 }	t_errors;
 
-void    ft_error(int n);
+void	ft_error(int n);
 
 #endif
